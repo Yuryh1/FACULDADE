4dda98204c18a24d92237e37e450b771afcd6327
Yury Samuel <Yuryh084@gmail.com>
2022-06-10 18:52:11 -0300

    Add files via upload

---
+++ testejpa/src/main/java/testejpa/RepositorioCliente.java
@@ -0,0 +1,42 @@
+package testejpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class RepositorioCliente {
+	
+	EntityManagerFactory emf;
+	EntityManager em;
+	
+	public RepositorioCliente() {
+		emf = Persistence.createEntityManagerFactory("minha-persistence-unit");
+		em = emf.createEntityManager();
+	}
+            
+	public void salvar(Cliente c) {
+		em.getTransaction().begin();
+		em.merge(c);
+		em.getTransaction().commit();
+		emf.close();
+	}
+	
+	public void remover(Cliente c ) {
+		em.getTransaction().begin();
+		em.merge(c);
+		em.getTransaction().commit();
+		emf.close();
+	}
+	@SuppressWarnings("unchecked")
+	public List<Cliente> listarTodos(){
+		em.getTransaction().begin();
+		 Query consulta = em.createQuery("select cliente from Cliente cliente");
+		 List<Cliente>clientes = consulta.getResultList();
+		 em.getTransaction().commit();
+		 emf.close();
+		return clientes;
+	}
+}
